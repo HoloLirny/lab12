5606465390fda56c08cb97597644ee5ba7a53d8c
Holo <korarit_p@cmu.ac.th>
2023-01-06 15:49:16 +0700

    lab12_completev.2

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,5 @@
     #include<iostream>
     #include<string>
-    #include<ctime>
     #include<cstdlib>
 
     using namespace std;
@@ -8,16 +7,13 @@
     string n[9]={"A","B+","B","C+","C","D+","D","F","W"};
 
     string roll(){
-        srand(time(0));
-        return n[rand()%9];
+        return n[rand()%9+1];
     }
 
     int main(){
-        srand(time(0));
         cout << "Press Enter 3 times to reveal your future.";
         cin.get();
         cin.get();
         cin.get();
         cout << "You will get "<<roll()<<" in this 261102.";
-        return 0;
     }
